61121a20d9ec6db482db015c21c956a3e96fc46f
Hendrik Haugg <hendrikhaugg@vermessungmuenchen.de>
2024-01-09 00:00:02 +0100

    eval

--- mmseg/test.py
+++ mmseg/test.py
@@ -1,5 +1,22 @@
 from mmseg.apis import MMSegInferencer
+import cv2
+
+## Load models into memory
+# inferencer = MMSegInferencer(model='pspnet_r18-d8_4xb4-80k_loveda-512x512')
+# # Inference
+# inferencer('demopics/32686_5337_masked.tif', show=True)
+
+# # Load models into memory
+# inferencer = MMSegInferencer(model='deeplabv3plus_r18-d8_4xb4-80k_loveda-512x512')
+# # Inference
+# inferencer('demopics/32686_5337.tif', show=True)
+
+# # Load models into memory
+# inferencer = MMSegInferencer(model='fcn_hr18s_4xb4-80k_loveda-512x512')
+# # Inference
+# inferencer('demopics/32686_5337.tif', show=True)
+
 # Load models into memory
-inferencer = MMSegInferencer(model='pspnet_r18-d8_4xb4-80k_loveda-512x512')
+inferencer = MMSegInferencer(model='fcn_hr18s_4xb4-80k_vaihingen-512x512')
 # Inference
-inferencer('demopics/32691_5335_masked.tif', show=True)
+inferencer('demopics/32686_5337.tif', show=True)
